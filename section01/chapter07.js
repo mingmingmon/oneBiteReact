1beb1387c307cea19bba239d3b701376b5f5e41c
mingmingmon <mingmingmon@naver.com>
2024-04-05 20:43:02 +0900

    first commit

---
+++ section01/chapter07.js
@@ -0,0 +1,27 @@
+// 1. 대입 연산자
+let var1 = 1;
+
+// 2. 산술 연산자
+let num1 = 3 + 2;
+let num6 = (1 + 2) * 10;
+
+// 3. 복합 대입 연산자
+let num7 = 10;
+num7 += 20;
+
+// 4. 증감 연산자
+let num8 = 10;
+num8++;
+
+// 5. 논리 연산자
+let or = true || false;
+let and = true && false;
+let not = !true;
+
+// 6. 비교 연산자
+let comp1 = 1 === 2;
+let comp2 = 1 !== 2;
+// == 은 자료형까지 비교 안 함. === 이 내가 알고 있는 비교연산
+
+let comp3 = -3 < -2 && -2 < -1;
+console.log(comp3);
